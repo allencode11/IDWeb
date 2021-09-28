2e115e3837b9936ef684555e525a1909f9b88b76
allencode11 <al.el.en.lina@gmail.com>
2021-09-28 16:09:00 +0300

    adding main.cpp file to C++ branch

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+main.cpp [unix] (01:59 01/01/1970)                                     0,0-1 All
+-- VISUAL BLOCK --                                                   1x1
+
